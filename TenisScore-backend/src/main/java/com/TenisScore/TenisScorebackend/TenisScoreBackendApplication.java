25d6a5537fe0c20dde8dd714db05dce8bdf5ddf0
Purice <andrei.purice1203@gmail.com>
2024-04-21 19:21:34 +0300

    second commit

---
+++ TenisScore-backend/src/main/java/com/TenisScore/TenisScorebackend/TenisScoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.TenisScore.TenisScorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenisScoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenisScoreBackendApplication.class, args);
+	}
+
+}
---
+++ TenisScore-backend/src/test/java/com/TenisScore/TenisScorebackend/TenisScoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TenisScore.TenisScorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenisScoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
